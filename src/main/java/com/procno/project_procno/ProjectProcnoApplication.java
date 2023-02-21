ba836e0247341fcd544ba36de74f459c7346aa02
David Daza <drdaza18@gmail.com>
2023-02-21 10:22:23 +0100

    added basic brackend structure

---
+++ src/main/java/com/procno/project_procno/ProjectProcnoApplication.java
@@ -0,0 +1,13 @@
+package com.procno.project_procno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectProcnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectProcnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/procno/project_procno/ProjectProcnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procno.project_procno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectProcnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
