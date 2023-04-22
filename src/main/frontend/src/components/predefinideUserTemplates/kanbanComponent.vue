53cb9574c981519eba3083b37c70fa6acfe5e4c6
David Daza <drdaza18@gmail.com>
2023-04-22 15:10:33 +0200

    added mixins changes

--- src/main/frontend/src/components/predefinideUserTemplates/kanbanComponent.vue
+++ src/main/frontend/src/components/predefinideUserTemplates/kanbanComponent.vue
@@ -1,16 +1,28 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+
 const props = defineProps({
     kanban:{
         type: Object as any
     }
 })
+
 </script>
 <template>
     <div class="kanban-container">
         <div class="state-box" v-for="state of kanban.container.elements">
             <h4>{{ state.name }}</h4>
         </div>
+
     </div>
 <!-- {{ kanban.title }} -->
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../../assets/styles/main' as *;
+.kanban-container{
+    @include gridDisplay(1, auto-fit);
+    width: 100%;
+    height: 100%;
+   
+}
+</style>
